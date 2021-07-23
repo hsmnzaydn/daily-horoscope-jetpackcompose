c84dffd43a52e186bced964819145fa6be982bf7
hsmnzaydn <serkan.zaydn@gmail.com>
2021-07-24 01:10:52 +0300

    added preview for widgets and splash screen

--- app/src/main/java/com/hsmnzaydn/daily_horoscope/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/hsmnzaydn/daily_horoscope/ui/screens/splash/SplashScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.hsmnzaydn.daily_horoscope.ui.theme.Purple200
@@ -20,6 +22,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import com.hsmnzaydn.daily_horoscope.R
+import com.hsmnzaydn.daily_horoscope.fake.NavControllerAndSplashViewModelProvider
 
 @Composable
 fun SplashScreen(
@@ -44,6 +47,36 @@ fun SplashScreen(
             delay(3000L)
             navController.popBackStack()
             navController.navigate(Navigation.HOROSCOPE_LIST_SCREEN.name)
+
+        }
+
+    }
+
+}
+@Preview
+@Composable
+fun SplashScreenPreview(
+    @PreviewParameter(
+        NavControllerAndSplashViewModelProvider::class) fakeParameter: Pair<NavController,SplashViewModel>
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight(1f)
+            .fillMaxWidth(1f),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(painter = painterResource(id = R.drawable.ic_logo),colorFilter = ColorFilter.tint(
+            Purple200), contentDescription = null)
+        Text(
+            stringResource(id = R.string.app_name),
+            Modifier.padding(top = 8.dp),
+            style = Typography.h1
+        )
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(3000L)
+            fakeParameter.first.popBackStack()
+            fakeParameter.first.navigate(Navigation.HOROSCOPE_LIST_SCREEN.name)
 
         }
 
--- app/src/main/java/com/hsmnzaydn/daily_horoscope/ui/widgets/HoroscopeDetailWidget.kt
+++ app/src/main/java/com/hsmnzaydn/daily_horoscope/ui/widgets/HoroscopeDetailWidget.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hsmnzaydn.daily_horoscope.horoscope.domain.entities.Description
 import com.hsmnzaydn.daily_horoscope.ui.theme.Typography
@@ -12,4 +13,10 @@ import com.hsmnzaydn.daily_horoscope.ui.theme.Typography
 fun HoroscopeDetailWidget(description: Description){
     Text(text = description.title,Modifier.padding(top = 16.dp,start = 8.dp,end = 8.dp))
     Text(text = description.description,Modifier.padding(top = 8.dp,start = 8.dp,end = 8.dp),style = Typography.body2)
+}
+
+@Preview
+@Composable
+fun detailListPreview(){
+   HoroscopeDetailWidget(description = Description("Daily Horoscope","Daily")) 
 }
